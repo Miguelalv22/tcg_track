d61088d2e3f4db0022b5c73af6107c732bcbc110
Miguel Lopez <miguel.2211.lopez@gmail.com>
2026-06-24 16:25:25 -0600

    feat:  add home, card_collection pages and link them

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
 from nicegui import ui
+from pages.home import create_home
+from pages.card_collection import create_card_collection
 
-with ui.header(elevated=True).style('background-color: #23252e').classes('items-center justify-between'):
-    ui.input(placeholder='search poke').style('background-color: white').classes('rounded-lg text-center')
-    ui.button('Search', on_click=lambda: ui.notify('You clicked me!')).classes('rounded-lg')
+@ui.page('/')
+def home():
+    create_home()
 
-with ui.card().tight().classes('w-60 shadow-lg rounded-lg overflow-hidden'):
-    # Card Image
-    ui.image('https://images.pokemontcg.io/xy1/1_hires.png').classes('h-55')
-    
-    # Card Content
-    with ui.card_section().classes('p-2'):
-        ui.label('Beautiful Mountains').classes('text-2xl font-bold text-gray-800')
-        ui.label('Exp.').classes('text-gray-600 mt-2')
-        
-    # Card Actions / Buttons
-    with ui.card_section().classes('bg-gray-50 p-2 flex justify-center gap-2 border-t'):
-        ui.button('View Details').classes('text-sm')
+@ui.page('/card-collection')
+def collection():
+     create_card_collection()
 
 ui.run()
---
+++ pages/card_collection.py
@@ -0,0 +1,7 @@
+from nicegui import ui
+from components.header import create_header
+from components.card import create_card
+
+def create_card_collection():
+      create_header() 
+      create_card()
